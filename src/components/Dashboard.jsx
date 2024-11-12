0c4f2e4ff039a653e7c8470796edf27c0c75563e
Ibrahim Itani <I.itani2002@gmail.com>
2024-11-13 01:30:44 +0200

    react<>fixed className naming

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -51,47 +51,47 @@ const Dashboard = () => {
 
   })
   return (
-    <div class="data-section">
-                <div class="features-card">
-                    <h2>In Finanz you can:</h2>
-                    <ul>
-                        <li>Manage your finances (create,edit,delete,...)</li>
-                        <li>View Expenses & Incomes</li>
-                        <li>Filter your transactions</li>
-                    </ul>
-                </div>
-                <div class="data-container">
-                    <div class="double-side-container">
-                        <div class="double-side-card">
-                            <h2>Monthly Budget</h2>
-                            <div class="overview-data">
-                                <p>Total: $<span id="budget-display"></span></p>
-                            </div>
-                        </div>
-                        <div class="double-side-card">
-                            <h2>Remaining Budget</h2>
-                            <div class="overview-data">
-                                <p>Total: $<span id="remaining-budget-display"></span></p>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="double-side-container">
-                        <div class="double-side-card">
-                            <h2>Expenses</h2>
-                            <div class="overview-data">
-                                <p>Total: $<span id="expenses-total-display"></span></p>
-                            </div>
-                        </div>
-                        <div class="double-side-card">
-                            <h2>Income</h2>
-                            <div class="overview-data">
-                                <p>Total: $<span id="income-total-display"></span></p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+    <div className="data-section">
+      <div className="features-card">
+        <h2>In Finanz you can:</h2>
+        <ul>
+          <li>Manage your finances (create, edit, delete,...)</li>
+          <li>View Expenses & Incomes</li>
+          <li>Filter your transactions</li>
+        </ul>
+      </div>
+      <div className="data-container">
+        <div className="double-side-container">
+          <div className="double-side-card">
+            <h2>Monthly Budget</h2>
+            <div className="overview-data">
+              <p>Total: ${budget}</p>
             </div>
-  )
-}
+          </div>
+          <div className="double-side-card">
+            <h2>Remaining Budget</h2>
+            <div className="overview-data">
+              <p>Total: ${remainingBudget}</p>
+            </div>
+          </div>
+        </div>
+        <div className="double-side-container">
+          <div className="double-side-card">
+            <h2>Expenses</h2>
+            <div className="overview-data">
+              <p>Total: ${totalExpenses}</p>
+            </div>
+          </div>
+          <div className="double-side-card">
+            <h2>Income</h2>
+            <div className="overview-data">
+              <p>Total: ${totalIncomes}</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Dashboard

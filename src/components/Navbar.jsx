456690f108c686d6c54d20058505a112c2b5340a
Ibrahim Itani <I.itani2002@gmail.com>
2024-11-12 21:55:34 +0200

    react <> added Navbar code from previous version

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
-
+import "../styles/dashboard.css"
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div class="side-bar">
+            <div class="side-logo">
+                <img src="/assets/logo.png" alt="logo"/>
+            </div>
+            <div class="out-budget hidden" id="warning">
+                <h3>Out of Budget!!</h3>
+            </div>
+            <nav>
+                <a href="/pages/dashboard.html" class="current-page">Overview</a>
+                <a href="/pages/income.html">Income</a>
+                <a href="/pages/expense.html">Expenses</a>
+            </nav>
+            <button class="reset" id="reset-button">Reset</button>
+            <em> &copy Finanz | Ibrahim Itani</em>
+    </div>
   )
 }
 

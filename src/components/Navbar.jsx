b82a0655cec09fb9cc33aaea8806b28cecfab9e6
Ibrahim Itani <I.itani2002@gmail.com>
2024-11-12 22:29:58 +0200

    react<>added mobile nav bar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,6 +3,7 @@ import "../styles/base.css"
 
 const Navbar = () => {
   return (
+    <>
     <div className="side-bar">
             <div className="side-logo">
                 <img src={"./assets/logo.png"} alt="logo"/>
@@ -18,6 +19,26 @@ const Navbar = () => {
             <button className="reset" id="reset-button">Reset</button>
             <em> &copy Finanz | Ibrahim Itani</em>
     </div>
+     <div className="top-section">
+     <h1>Hi,  <span id="username-display"></span>!</h1>
+     <div className="mobile-bar">
+         <div className="burger-logo" id="mobile-menu">
+             <img src="/assets/burger-icon.svg" alt=""/>
+         </div>
+         <div className=" mobile hidden" id="mobile-content">
+             <div className="out-budget hidden" id="warning">
+                 <h3>Out of Budget!!</h3>
+             </div>
+             <nav>
+                 <a href="/pages/dashboard.html" className="current-page">Overview</a>
+                 <a href="/pages/income.html">Income</a>
+                 <a href="/pages/expense.html">Expenses</a>
+             </nav>
+             <button className="reset" id="reset-button">Reset</button>
+         </div>
+     </div>
+ </div>
+ </>
   )
 }
 

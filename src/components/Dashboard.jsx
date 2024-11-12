97f7fffaed1e59b28bda56a53d58057ab951e18c
Ibrahim Itani <I.itani2002@gmail.com>
2024-11-13 01:29:33 +0200

    react<>replaced vanilla DOM js with useState variables values

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -3,71 +3,30 @@ import "../styles/dashboard.css";
 import axios from "axios";
 
 const Dashboard = () => {
-    const remainingBudget = document.getElementById("remaining-budget-display");
-const totalExpensesDisplay = document.getElementById("expenses-total-display");
-const totalIncomeDisplay = document.getElementById("income-total-display");
-const budgetWarning = document.getElementById("warning");
-const resetButton = document.getElementById("reset-button");
-const userNameDisplay = document.getElementById("username-display");
-const budgetDisplay = document.getElementById("budget-display");
+  const [username, setUsername] = useState("");
+  const [budget, setBudget] = useState(0);
+  const [totalExpenses, setTotalExpenses] = useState(0);
+  const [totalIncomes, setTotalIncomes] = useState(0);
+  const [remainingBudget, setRemainingBudget] = useState(0);
 
-axios
-  .post("http://localhost/finanz-tracker-enhanced/apis/displayUser.php",
-    {
-      id: localStorage.getItem("currentUser"),
-    },
-    {
-      headers: {
-        "Content-Type": "application/json",
-      },
-    }
-  )
-  .then((response) => {
-    userNameDisplay.innerText = response.data.name;
-    budgetDisplay.innerText = response.data.budget;
-  })
-  .catch((err) => console.log(err));
-
-axios
-  .post("http://localhost/finanz-tracker-enhanced/apis/totalValues.php",
-    {
-      id: localStorage.getItem("currentUser"),
-    },
-    {
-      headers: {
-        "Content-Type": "application/json",
-      },
-    }
-  )
-  .then((response) => {
-    totalExpensesDisplay.innerText = response.data.total_expenses;
-    totalIncomeDisplay.innerText = response.data.total_income;
-    remainingBudget.innerText =
-      parseInt(budgetDisplay.innerText) -
-      parseInt(totalExpensesDisplay.innerText);
-    if(parseInt(remainingBudget.innerText)<=0){
-      budgetWarning.classList.toggle("hidden")
-    }
-  })
-  .catch((err) => console.log(err));
-  axios
-  .post(
-    "http://localhost/finanz-tracker-enhanced/apis/displayUser.php",
-    {
-      id: localStorage.getItem("currentUser"),
-    },
-    {
-      headers: {
-        "Content-Type": "application/json",
-      },
-    }
-  )
-  .then((response) => {
-    userNameDisplay.innerText = response.data.name;
-    budgetDisplay.innerText = response.data.budget;
-
-  })
-  .catch((err) => console.log(err));
+  useEffect(() => {
+    axios
+      .post(
+        "http://localhost/finanz-tracker-enhanced/apis/displayUser.php",
+        {
+          id: localStorage.getItem("currentUser"),
+        },
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      )
+      .then((response) => {
+        setUsername(response.data.name);
+        setBudget(response.data.budget);
+      })
+      .catch((err) => console.log(err));
 
   resetButton?.addEventListener("click",()=>{
     axios

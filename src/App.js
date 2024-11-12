2f8e13802abe78b6fd65c6b34591c789089618b9
Ibrahim Itani <I.itani2002@gmail.com>
2024-11-12 23:16:07 +0200

    react<>implemented routes in App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
-import Dashboard from './components/Dashboard';
-import Navbar from './components/Navbar';
-import './styles/base.css';
+import { Route, Routes } from "react-router-dom";
+import Dashboard from "./components/Dashboard";
+import Navbar from "./components/Navbar";
+import "./styles/base.css";
+import Login from "./components/Login";
+import Data from "./components/Data";
 
 function App() {
   return (
-    <div className='virtual-body'>
-      <Navbar/>
-      <Dashboard/>
+    <div className="virtual-body">
+      {window.location.pathname !== "/" && <Navbar />}
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="home" element={<Dashboard />} />
+        <Route path="income" element={<Data dataType="income"/>}/>
+        <Route path="expense" element={<Data dataType="expense"/>}/>
+      </Routes>
     </div>
   );
 }
